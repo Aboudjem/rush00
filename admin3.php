39c914212e7b2d87136545918d3357857f193154
Adam BOUDJEMAA <aboudjem@e1r10p15.42.fr>
2017-06-18 11:58:44 +0200

    push

--- admin3.php
+++ admin3.php
@@ -11,11 +11,29 @@ $file = "./private/passwd";
 $user = unserialize(file_get_contents($file));
 $cat = unserialize(file_get_contents("private/cat.csv"));
 ?>
-<?php include('./tab/add_article.php'); ?>
-<?php include('./tab/add_cat.php'); ?>
-<?php include('./tab/delete_cat.php'); ?>
-<?php include('./tab/delete_user.php'); ?>
-<?php include('./tab/modif_cat.php'); ?>
+<form action="admin3.php" method="POST">
+    <select name="name">
+    <option value="article">article</option>
+      <option value="cat">cat</option>
+      <option value="user">user</option>
+  </select>
+  <input style="margin-top: 25px; font-size: 20px;" type="submit" name="submit" value="OK">
+</form>
+<?php
+if ($_POST['name'] == "article")
+{
+  include('./tab/add_article.php'); 
+
+}
+else if ($_POST['name'] == "cat")
+{
+ include('./tab/add_cat.php'); 
+ include('./tab/modif_cat.php'); 
+ include('./tab/delete_cat.php'); 
+}
+else if ($_POST['name'] == "user")
+ include('./tab/delete_user.php'); 
+?>
 
 <hr>
 
--- articles.php
+++ articles.php
@@ -5,22 +5,14 @@
     <link rel="stylesheet" type="text/css" href="index.css">
     <title>Magasins</title>
 </head>
-
 <body>
-    <?php
-    if ($bdd = unserialize(file_get_contents('private/bdd.csv')))
-        foreach ($bdd as $key => $value) {
-            print($value['name']."");
-        }
-    ?>
-
-        <a href="saver.php?article=book"><div class="articles">
-            <div style="padding: 50px; text-align: center;">
+    <a href="saver.php?article=book"><div class="articles">
+        <div style="padding: 50px; text-align: center;">
             <h3 class="artName">Book</h3>
-                <span class="tartif" >43€</span><br/>
-                <?php if ($_SESSION["book"]){ ?><span> you have <?php echo $_SESSION["book"]; ?></span><?php } ?>
-            </div>
-        </a>
-    </div>
+            <span class="tartif" >43€</span><br/>
+            <?php if ($_SESSION["book"]){ ?><span> you have <?php echo $_SESSION["book"]; ?></span><?php } ?>
+        </div>
+    </a>
+</div>
 </body>
 </html>
