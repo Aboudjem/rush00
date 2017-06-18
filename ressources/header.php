64e6d30804705c0cea5946f9a02f72ee67d1274c
Amine KABBOUCH <akabbouc@e1r11p16.42.fr>
2017-06-18 12:37:36 +0200

    push

--- ressources/header.php
+++ ressources/header.php
@@ -6,10 +6,19 @@
     <div class="menu">
         <a href="index.php" class='menu-link'>Accueil</a>
         <a href="articles.php" class='menu-link'>Articles</a>
-        <a href="loginPage.php" class='menu-link'>Connexion</a>
-        <a href="registerPage.php" class='menu-link'>S'enregistrer</a>
-        <a href="panier.php" class='menu-link'>Panier</a>
-        <?php if ($_SESSION['loggued_on_user']) { ?><a href="#" class='menu-link'><?php echo $_SESSION['loggued_on_user']; ?></a><?php } ?>
+        <?php 
+        	if ($_SESSION['loggued_on_user'])
+        	{
+	       		echo "<a href='logout.php' class='menu-link'>Logout</a>";
+         		echo "<a href='#' class='menu-link'>".$_SESSION['loggued_on_user']."</a>"; 
+            }
+            else
+            {
+    	   		echo "<a href='loginPage.php' class='menu-link'>Connexion</a>";
+       			echo "<a href='registerPage.php' class='menu-link'>S'enregistrer</a>";
+        	}
+        		echo "<a href='panier.php' class='menu-link'>Panier</a>";
+        ?>
     </div>
 </header>
 </htlm>
