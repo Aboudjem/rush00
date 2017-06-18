1ea1c2874beb07af1253a47b0352b87e76d08ba8
Heiji <julien.juret@gmail.com>
2017-06-18 01:41:39 +0000

    admin panel

--- admin.php
+++ admin.php
@@ -1,31 +1,38 @@
 <?php
 session_start();
+$file = "./private/passwd";
+$user = unserialize(file_get_contents($file));
+$tab = [];
 ?>
+<?php
+foreach ($user as $k => $v)
+{
+	?>
+	<!DOCTYPE html>
 <html>
-<head>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <link rel="stylesheet" type="text/css" href="index.css">
-</head>
-<header>
-    <div class="menu">
-        <a href="index.html" class='menu-link'>Accueil</a>
-        <a href="login.html" class='menu-link'>Connexion</a>
-        <a href="register.html" class='menu-link'>S'enregistrer</a>
-        <a href="panier.html" class='menu-link'>Panier</a>
-    </div>
-</header>
-<body>
-    <div class="form_login">
-        <h1>Admin</h1>
-        <?php
-        print("Vous etes <b><span style='color:#f21d5d; font-size:30px;'>".$_SESSION['loggued_on_user']."</span><br>");
-        if (!file_exists("private/passwd"))
-            file_put_contents("private/passwd", "");
-        if($bdd = unserialize(file_get_contents("private/passwd")))
-            foreach($bdd as $i => $v)
-                print("Login --> <b><span style='color:#f21d5d; font-size:30px;'>[".$v['login']."]</b> MdP --> <b>[".$v['passwd']."]</b></span></br>");
-                    ?>
-            </div>
-        </body>
-        </html>
-
+<body style="margin:auto;">
+<table style="background-color: rgb(35,35,35);" width="70%" class="text" border="1px">
+	<tr>
+    	<td class="tr_admin">
+    		<div class="test">
+		    <p>User:<span class="login"> <?php echo $v['login']?></span></p>
+		    <form action='delete.php' method='post' style="padding-right: 10px;">
+				<input type='hidden' name='username' value="<?php echo $v['login'] ?>"/>
+				<input style="margin-top: 25px; font-size: 50px;" type='submit' value='delete'>
+		    </form>
+		    <br/><br/>
+		    <h4></h4>
+		    <form action='SaveArticle.php' method='post' style="padding-right: 10px;">
+				<input type='hidden' name='username' value="<?php echo $v['login'] ?>"/>
+				<input style="margin-top: 25px; font-size: 50px;" type='submit' value='delete'>
+		    </form>
+		    <br/><br/>>
+		    </div>
+    	</td>
+    </tr>
+    </table>
+</body>
+</html>
+    <?php
+}
+?>
