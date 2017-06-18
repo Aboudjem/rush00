4f8e98646f88265fba4e715260adc0770755d661
Adam BOUDJEMAA <aboudjem@e1r10p15.42.fr>
2017-06-18 12:03:25 +0200

    push

--- modif_cat.php
+++ modif_cat.php
@@ -1,21 +1,23 @@
 <?PHP
+session_start();
 if ($_POST['oldcat'] !== NULL && $_POST['newcat'] !== NULL && 
 	$_POST['newcat'] !== "" && $_POST['submit'] === "Modifier"
 	&& file_exists('./private/cat.csv'))
 {
-	if ($cat = unserialize(file_get_contents('./private/cat.csv')))
+	$cat = unserialize(file_get_contents('./private/cat.csv'));
 	{
 		foreach($cat as $k => $v)
-			if ($v['name'] === $_POST['newcat'])
+		{
+			if ($v['name'] === $_POST['oldcat'])
+				$cat[$k]['name'] = $_POST['newcat'];
+			else if ($v['name'] === $_POST['newcat'])
 			{
 				header('Location: admin3.php');
 				echo 'Categorie deja existante';
 				exit();
 			}
-			if ($v['name'] === $_POST['oldcat'])
-				$tmp = $v;
+		}
 	}
-	$cat[$tmp]['name'] = $_POST['newcat'];
 	file_put_contents("./private/cat.csv", serialize($cat));
 }
 header('Location: admin3.php');
