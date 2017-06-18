48bc982cec92316ce4c6a8d7463db597d77b5683
Amine KABBOUCH <akabbouc@e1r11p16.42.fr>
2017-06-18 10:16:33 +0200

    push

--- admin3.php
+++ admin3.php
@@ -34,16 +34,58 @@ foreach ($user as $k => $v)
 <hr>
 <table border="1px">
 	<tr>
-    	<td class="tr_admin">
+    	<td style="text-align: center;">
     		<div class="cate">
                 <form action='add_cat.php' method='post' style="padding-right: 10px;">
-                    <span class="name_cat">Ajout Categorie </span><input type="text" name="cat">
+                    <span class="name_cat">Ajout Categorie </span>
+                    <input style="width: 300px;" type="text" name="cat">
 				    <input style="margin-top: 25px; font-size: 20px;" type='submit' name='submit' value='OK'>
                 </form>
 		    </div>
     	</td>
     </tr>
 </table>
+<table style="background-color: rgb(35,35,35);" width="70%" class="text" border="1px">
+    <tr>
+        <td>
+            <div class="cate">
+                <form action='del_cat.php' method='post' style="padding-right: 10px;">
+                    <span class="name_cat">Supprimer la categorie</span>
+                    <SELECT style="font-size: 20px; margin-left: 20px; margin-top: 10px; width: 100px;" name= 'cat' size="1">
+                        <?PHP foreach ($cat as $k => $v)
+                        echo '<OPTION>'.$cat[$k]['name']; ?>
+                    </SELECT>
+                    <input style="margin-top: 25px; font-size: 20px;" type='submit' name='submit' value='Supprimer'>
+                </form>
+            </div>
+        </td>
+    </tr>
+    </table>
+<table style="background-color: rgb(35,35,35);" width="70%" class="text" border="1px">
+    <tr>
+        <td style="text-align: center;">
+            <form action='modif_cat.php' method='post' style="padding-right: 10px;">
+                <span class="name_cat">Modifier la categorie</span>
+        </td>
+        <td>
+                <SELECT style="font-size: 20px; margin-left: 240px; width: 150px;" name= 'oldcat' size="1">
+                    <?PHP foreach ($cat as $k => $v)
+                    echo '<OPTION>'.$cat[$k]['name']; ?>
+                    </SELECT>
+        </td>
+    </tr>
+    <tr>                    
+        <td style="text-align: center;"">
+                <span style="text-align: start;" class="name_cat">Nom modifie:</span>
+        </td>
+        <td>
+                <input style="width: 390px; height: 30px;" type="text" name="newcat">
+        </td>
+            </form>
+    </tr>
+    </table>
+
+            <input style="margin-top: 25px; font-size: 30px; display: flex;" type='submit' name='submit' value='Modifier'>    
 <hr>
 <table border="1px">
     <tr>
